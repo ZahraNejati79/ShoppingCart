919650855ea54cc404efd759757b8fc223226b2f
Zahra Nejati <nejatizahra4@gmail.com>
2022-08-13 12:08:35 +0430

    basic axios http

---
+++ src/services/HttpService.jsx
@@ -0,0 +1,12 @@
+const { default: axios, default: axios } = require("axios");
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  delete: axios.delete,
+  put: axios.put,
+};
+
+export default http;
